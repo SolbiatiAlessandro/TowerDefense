d94358fd8ae232f762c26aa62d46b3a4866f2990
Alessandro Solbiati <lessandro@fb.com>
2022-02-04 22:57:26 +0000

    human untested

--- src/prefabs/Human.js
+++ src/prefabs/Human.js
@@ -7,8 +7,8 @@ export default class Human extends Phaser.Plugin.Isometric.IsoSprite {
 		this.animations.play('idle', 2, true);
 
 		this.anchor.setTo(.5, .5);
-		//this.enemies = enemies;
-		//this.arrows = arrows;
+		this.enemies = enemies;
+		this.arrows = arrows;
 
 		this.showInterval = 400;
 		this.shotTime = this.game.time.now + this.shotInterval;
@@ -16,10 +16,21 @@ export default class Human extends Phaser.Plugin.Isometric.IsoSprite {
 
 	update(){
 		if(this.game.time.now > this.shotTime){
-			//this.target = this.enemies.findNearest(this.x, this.y);
-			console.log("shoot");
+			this.target = this.enemies.findNearest(this.x, this.y);
 
+			if(this.target){
+				var arrow = this.arows.getFirstDead();
+				if(!arrow) arrow = this.arrows.create(0, 0, 'arrow');
+				arrow.revive();
+				arrow.x = this.x;
+				arrow.y = this.y;
+				arrow.lifespan = 4000;
+
+				arrow.rotation = this.game.physics.arcade.moveToObject(
+					arrow, this.target, 120);
+				
+				this.shotTime = this.game.time.now + this.showInterval;
+			}
 		}
 	}
-
 }
