bcd401bfdd796f78ab04d09c89db9ca4ecb0cf3b
Alessandro Solbiati <lessandro@fb.com>
2022-02-08 21:30:30 +0000

    tilemap

--- src/states/Game.js
+++ src/states/Game.js
@@ -15,6 +15,7 @@ export default class Game extends Phaser.State {
   }
 
   create() {
+      this.mapData =  this.game.cache.getJSON('mapdata');
 	  this.game.physics.startSystem(Phaser.Physics.ARCADE)
 	  this.game.time.advancedTiming = true;
 	  this.game.add.sprite(0, 0, "gamebg");
@@ -27,11 +28,41 @@ export default class Game extends Phaser.State {
 	  this.allies.add(_human);
 	  var _skeleton = new Skeleton(this.game, 300, 200);
 	  this.allies.add(_skeleton);
+	  this.spawnTiles();
 	  
   }
 
 
   update() {
+  }
+
+  spawnTiles(){
+	  var size = 55;
+	  var mapWidth = this.mapData.tileMap[0].length - 1;
+	  var mapHeight = this.mapData.tileMap.length - 1;
+	  this.gameTiles = [];
+
+	  var i=0, tile;
+	  for (var y = 0; y <= mapHeight; y++){
+
+		  this.gameTiles[y] = [];
+
+		  for(var x = 0; x <= mapWidth; x++){
+			  var tileNumber = this.mapData.tileMap[y][x];
+			  var tileName = this.mapData.tileNames[tileNumber];
+			  tile = this.game.add.isoSprite(
+				  x*size,
+				  y*size,
+				  0,
+				  tileName,
+				  0,
+				  this.isoGroup
+			  );
+			  tile.anchor.set(0.5, 0);
+			  tile.buyable = (tileNumber == 0) ? true : false;
+			  this.gameTiles[y][x] = tile;
+		  }
+	  }
   }
 
 
