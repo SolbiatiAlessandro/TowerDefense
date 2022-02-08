be8734b35e984918629cd5c4d3e1221e771601a9
Alessandro Solbiati <lessandro@fb.com>
2022-02-08 22:42:41 +0000

    tile selection

--- src/states/Game.js
+++ src/states/Game.js
@@ -57,17 +57,44 @@ export default class Game extends Phaser.State {
 
 	  //
 	  // TEST
-	  //var _human = new Human(this.game, 200, 200);
-	  //this.allies.add(_human);
-	  //var _skeleton = new Skeleton(this.game, 300, 200);
-	  //this.allies.add(_skeleton);
+	  var _human = new Human(this.game, 200, 200);
+	  this.allies.add(_human);
+	  var _skeleton = new Skeleton(this.game, 300, 200);
+	  this.allies.add(_skeleton);
 
 	  this.spawnTiles();
-	  
+
+	  this.easystar = new EasyStar.js();
+	  this.easystar.setGrid(this.mapData.tileMap);
+	  this.easystar.setAcceptableTiles([1]);
+	  this.boundFound = this.pathFound.bind(this);
+	  this.easystar.findPath(1, 0, 0, 4, this.boundFound);
+
+	  this.cursorPos = new Phaser.Plugin.Isometric.Point3();
+	  this.nextSpawn = this.game.time.now + this.spawnTime;
   }
 
 
   update() {
+	  this.game.iso.unproject(this.game.input.activePointer.position, this.cursorPos);
+	  this.isoGroup.forEach(this.checkTiles, this, false);
+  }
+
+
+  checkTiles(tile){
+	  var inBounds = tile.isoBounds.containsXY(this.cursorPos.x, this.cursorPos.y);
+
+	  if(!tile.selected && inBounds){
+		  tile.selected = true;
+		  tile.tint = 0x86bfda;
+		  this.game.add.tween(tile).to( {isoZ: 4} , 200, Phaser.Easing.Quadratic.InOut, true );
+		  this.selectedTile = tile;
+	  } else if (tile.selected && !inBounds){
+		  tile.selected = false;
+		  tile.tint = 0xffffff;
+		  this.game.add.tween(tile).to({isoZ: 0}, 200, Phaser.Easing.Quadratic.InOut, true);
+	  }
+
   }
 
   spawnTiles(){
@@ -95,6 +122,17 @@ export default class Game extends Phaser.State {
 			  tile.anchor.set(0.5, 0);
 			  tile.buyable = (tileNumber == 0) ? true : false;
 			  this.gameTiles[y][x] = tile;
+		  }
+	  }
+  }
+
+  pathFound(path){
+	  if (path != null){
+		  this.convertedPath = [];
+		  var curPoint;
+		  for(var i = 0; i < path.length; i++){
+			  curPoint = this.gameTiles[path[i].y][path[i].x];
+			  this.convertedPath.push( {x: curPoint.isoX, y: curPoint.isoY} );
 		  }
 	  }
   }
